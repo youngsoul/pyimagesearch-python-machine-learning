c60383d2968120f906887ee97aa53bc9ac8e6f64
patrickryan <theyoungsoul@gmail.com>
2019-01-17 08:28:24 -0500

    formatting

--- classify_images.py
+++ classify_images.py
@@ -22,15 +22,16 @@ from rgbhistogram import RGBHistogram
 
 
 def extract_color_stats(image):
-	# split the input image into its respective RGB color channels
-	# and then create a feature vector with 6 values: the mean and
-	# standard deviation for each of the 3 channels, respectively
-	(R, G, B) = image.split()
-	features = [np.mean(R), np.mean(G), np.mean(B), np.std(R),
-		np.std(G), np.std(B)]
+    # split the input image into its respective RGB color channels
+    # and then create a feature vector with 6 values: the mean and
+    # standard deviation for each of the 3 channels, respectively
+    (R, G, B) = image.split()
+    features = [np.mean(R), np.mean(G), np.mean(B), np.std(R),
+                np.std(G), np.std(B)]
 
-	# return our set of features
-	return features
+    # return our set of features
+    return features
+
 
 """
 --dataset /Users/patrickryan/Development/python/mygithub/pyimagesearch-python-machine-learning/3scenes
@@ -39,27 +40,25 @@ def extract_color_stats(image):
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", type=str, default="3scenes",
-	help="path to directory containing the '3scenes' dataset")
+                help="path to directory containing the '3scenes' dataset")
 ap.add_argument("-m", "--model", type=str, default="knn",
-	help="type of python machine learning model to use")
+                help="type of python machine learning model to use")
 args = vars(ap.parse_args())
 
-rgbHisto = RGBHistogram([8,8,8])
-
+rgbHisto = RGBHistogram([8, 8, 8])
 
 # define the dictionary of models our script can use, where the key
 # to the dictionary is the name of the model (supplied via command
 # line argument) and the value is the model itself
 models = {
-	"knn": KNeighborsClassifier(n_neighbors=1),
-	"naive_bayes": GaussianNB(),
-	"logit": LogisticRegression(solver="lbfgs", multi_class="auto"),
-	"svm": SVC(kernel="linear"),
-	"decision_tree": DecisionTreeClassifier(),
-	"random_forest": RandomForestClassifier(n_estimators=100),
-	"mlp": MLPClassifier()
+    "knn": KNeighborsClassifier(n_neighbors=1),
+    "naive_bayes": GaussianNB(),
+    "logit": LogisticRegression(solver="lbfgs", multi_class="auto"),
+    "svm": SVC(kernel="linear"),
+    "decision_tree": DecisionTreeClassifier(),
+    "random_forest": RandomForestClassifier(n_estimators=100),
+    "mlp": MLPClassifier()
 }
-
 
 # grab all image paths in the input dataset directory, initialize our
 # list of extracted features and corresponding labels
@@ -70,25 +69,25 @@ labels = []
 
 # loop over our input images
 for imagePath in imagePaths:
-	# load the input image from disk, compute color channel
-	# statistics, and then update our data list
-	image = Image.open(imagePath)
+    # load the input image from disk, compute color channel
+    # statistics, and then update our data list
+    image = Image.open(imagePath)
 
-	# using color stats does help along with rgbhisto
-	features = extract_color_stats(image)
+    # using color stats does help along with rgbhisto
+    features = extract_color_stats(image)
 
-	# Depending upon the algorithm, using the histogram is helpful
-	# check out mlp with and without histogram
-	# check out random forest with and without
-	histo = rgbHisto.read_describe(imagePath)
-	features.extend(histo.tolist())
+    # Depending upon the algorithm, using the histogram is helpful
+    # check out mlp with and without histogram
+    # check out random forest with and without
+    histo = rgbHisto.read_describe(imagePath)
+    features.extend(histo.tolist())
 
-	data.append(features)
+    data.append(features)
 
-	# extract the class label from the file path and update the
-	# labels list
-	label = imagePath.split(os.path.sep)[-2]
-	labels.append(label)
+    # extract the class label from the file path and update the
+    # labels list
+    label = imagePath.split(os.path.sep)[-2]
+    labels.append(label)
 
 # when you get here
 # every row in the data array is
@@ -100,7 +99,7 @@ labels = le.fit_transform(labels)
 # perform a training and testing split, using 75% of the data for
 # training and 25% for evaluation
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
-	test_size=0.25)
+                                                  test_size=0.25)
 
 model_name = args["model"]
 # train the model
@@ -113,7 +112,7 @@ print("[INFO] evaluating...")
 predictions = model.predict(testX)
 accuracy = accuracy_score(testY, predictions)
 class_report = classification_report(testY, predictions,
-	target_names=le.classes_)
+                                     target_names=le.classes_)
 
 print(f'Model: {model_name}')
 print(f'Accuracy: {accuracy}')
